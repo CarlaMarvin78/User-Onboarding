d8e6e3142fd503478cbadd6299c317ff2ad965dc
Carla Marvin <carlamarvin78@gmail.com>
2019-08-07 21:54:41 -0400

    Initial Commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withFormik, Form, Field } from "formik";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+              
+        <h1> Onboarding</h1>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/UserForm.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+
+  
+  export default UserForm;
